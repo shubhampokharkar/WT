74af8a4779cfb4e5e5640a3f344de491d5f9ea1a
shubhampokharkar <67820083+shubhampokharkar@users.noreply.github.com>
2020-11-05 15:23:13 +0530

    Add files via upload

---
+++ support.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MTPL</title>
+    <link rel="stylesheet" href="cproj.css">
+    <link rel="stylesheet" href="activatecss.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div class="head">
+        <div style="float:left ;color:yellow ; font-weight: bold;">M</div>
+        <div style="float:left ;color:blue ; font-weight: bold;">T</div>
+        <div style="float:left ;color:red ; font-weight: bold;">P</div>
+        <div style="float:left ;color:pink ; font-weight: bold;">L</div>
+        <div class="user" style="float: right; font-size: 30px; color:white; margin-right: 30px;"><i class="fas fa-user-circle"></i></div>
+    </div>>
+    <input type="checkbox" id="check">
+    <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-bars" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+        <header>MTPL</header>
+        <ul>
+            <li><a href="project.html"><i class="fas fa-search"></i>Search</a></li>
+            <li><a href="#" class="active"><i class="fas fa-calendar-week"></i>Activation</a></li>
+            <li>
+               <a href="#" class="Obtn"><i class="fas fa-cart-arrow-down"></i>Order
+               <span id="span" class="fas fa-caret-down first"></span>
+               </a>
+               <ul class="Odbtn">
+                   <li><a href="order.html" class="active">Change Order</a></li>
+               </ul>
+            </li>
+            <li><a href="Modify.html"><i class="fas fa-edit"></i>Modify</a></li>
+            <li><a href="#"><i class="fas fa-redo"></i>Renew</a></li>
+            <li><a href="#"><i class="fas fa-pause"></i>Suspend</a></li>
+            <li><a href="#"><i class="fas fa-male"></i>E-Support</a></li>
+            <li><a href="#"><i class="fas fa-power-off"></i>Logout</a></li>
+        </ul>
+    </div>
+    <section>
+        <div class="center">
+	<h1 align="center">The Imperial Business Hotel</h1>
+	<br>
+	<img src="cpp.png" alt="cp photo" width=250px height=250px style="display: block;
+	margin-left: auto;
+ 	margin-right: auto;">
+	<p align="center">Mr. Rohit Shevate</p>
+	<br>
+	<address align="center">
+	      Kohinoor Complex,Western Express Highway,<br>
+	      Opposite Rawalpada,Dahisar(East),<br>
+	      Mumbai - 400 068.
+	</address>
+	<br>
+	<p align="center"><b>Tel</b> : 020 56899933</p>
+	<br>
+	<p align="center"><b>Mobile no</b> : +91-9549504950</p>
+	<br>
+	<p align="center"><b>Email Id</b> : mtplcablenetwork@gmail.com</p>
+	<br><br><br><br>
+	</div>
+    </section>
+    <script src="Jscript.js"></script>
+</body>
+</html>
